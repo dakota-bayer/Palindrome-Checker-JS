90dee98f6f96e0dd03cc16b18dee17c543447c43
dakota-bayer <89050126+dakota-bayer@users.noreply.github.com>
2024-08-12 07:20:58 -0500

    script complete. need to test on fcc

--- script.js
+++ script.js
@@ -1,46 +1,54 @@
-const textInput = document.getElementById('text-input');
+const textInput = document.getElementById("text-input");
 const checkButton = document.getElementById("check-btn");
 const resultText = document.getElementById("result");
 
-//checkButton.addEventListener("click", checkPalindrome);
+checkButton.addEventListener("click", checkPalindrome);
 
 function checkPalindrome() {
-    console.log(`entered checkPalindrome(). Text in input is ${textInput.value}`);
+  if (textInput.value === "") {
+    alert("Please input a value");
+    return;
+  }
   const original = textInput.value;
   let cleaned = cleanStr(original);
   cleaned = cleaned.toLowerCase();
-  const isPalindrome = isPalindrome2(cleaned);
+  const isPalindrome = DetermineIfPalindrome(cleaned);
   printResult(original, isPalindrome);
 }
 
 function cleanStr(str) {
-    console.log(`entered cleanStr() for ${str}`);
-  const regex = /[.,_\s]/g;
+  const regex = /[.,_-\s]/g;
   return str.replace(regex, "");
 }
 
-function isPalindrome2(word) {
-    console.log(`entered isPalidrome2() for ${word}`);
-    // Is the characters in this string form a palindrome?
-  const k = word.length - 1;
-  console.log(`word.length is ${word.length}`)
-  for (let i = 0; i < word.lenth / 2; i++) {
-
-    console.log(`copmaring ${word.charAt(i)} and ${word.charAt(k)}`);
-    if(word.charAt(i) !== word.charAt(k)){
-        return false;
+function DetermineIfPalindrome(word) {
+  let k = word.length - 1;
+  for (let i = 0; i < Number(word.length) / 2; i++) {
+    --console.log(`copmaring ${word.charAt(i)} and ${word.charAt(k)}`);
+    const firstChar = word.charAt(i);
+    const secondChar = word.charAt(k);
+    const ignoreComparison = DetermineIfIgnore(firstChar, secondChar);
+    if (!ignoreComparison && firstChar !== secondChar) {
+      return false;
     }
     k--;
-
   }
   return true;
 }
 
-function printResult(word, isPalindrome) {
-    console.log("entered printResult()");
-  const resultStr = `${word} is ${isPalindrome ? "" : "not "}a palindrome`;
-  console.log(resultStr);
-  resultText.innerText = resultStr;
+function DetermineIfIgnore(firstChar, secondChar) {
+  if (
+    (firstChar == "(" && secondChar == ")") ||
+    (firstChar == ")" && secondChar == "(") ||
+    (firstChar == "/" && secondChar == "\\") ||
+    (firstChar == "\\" && secondChar == "/")
+  ) {
+    return true;
+  }
+  return false;
 }
 
-checkButton.addEventListener("click", checkPalindrome);
+function printResult(word, isPalindrome) {
+  const resultStr = `${word} is ${isPalindrome ? "" : "not "}a palindrome`;
+  resultText.innerText = resultStr;
+}
